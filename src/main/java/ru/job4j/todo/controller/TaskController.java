49f1e0df25143610d0740e2bf759ec539dcdece3
Pavel Valger <pavelwalker@mail.ru>
2023-09-07 00:33:36 +0500

    4. TODO list [#3786] Добавил статические ресурсы CSS в проект

---
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -0,0 +1,9 @@
+package ru.job4j.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tasks")
+public class TaskController {
+}
